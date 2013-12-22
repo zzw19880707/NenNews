4fa0e5eed69534e810be326bf67f870efd23a662
zzw19880707 <zzw414851474@qq.com>
2013-12-22 23:26:15 +0800

    增加通知key

--- PetInfo/PetInfo/kPetInfo.h
+++ PetInfo/PetInfo/kPetInfo.h
@@ -10,9 +10,13 @@
 #define PetInfo_kPetInfo_h
 
 #pragma mark NSNotificationCenter
-//首页加载时,定位完成的通知
-#define isEndLocation @"isEndLocation"
-#define isLoadHomeData @"isLoadHomeData"
+//夜间模式开启/关闭
+#define kNightModeChangeNofication @"kNightModeChangeNofication"
+//字体大小改变
+#define kFontSizeChangeNofication @"kFontSizeChangeNofication"
+#define kSizeBigNofication @"20"
+#define kSizeMiddleNofication @"20"
+#define kSizeSmallNofication @"20"
 
 #pragma mark UserDefaults
 //判断是否是第一次登陆
@@ -34,14 +38,10 @@
 #define NenNewsTextColor COLOR(88, 195, 241)
 #define NenNewsgroundColor COLOR(247, 247, 247)
 
-#define NenNews_day_Color COLOR(240,240,240)
-#define NenNews_day_text_Color COLOR(21,21,21)
-
-#define NenNews_night_COlor COLOR(31，32，36)
-#define NenNews_night_text_COlor COLOR(136，145，152)
 
 #pragma mark 文件名
 #define column_file_name @"column.plist"
 #define data_file_name @"data.plist"
-
+#define Night_file_name @"Night.plist"
+#define NightModel_file_name @"NightModel.plist"
 #endif
