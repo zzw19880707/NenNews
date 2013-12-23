e68d19dc60102da4590f59f4464371aa5a3d16e0
zzw19880707 <tenyea@tenyeatekiMac-mini.local>
2013-12-23 17:16:03 +0800

    添加数据中心

---
+++ PetInfo/PetInfo/DataCenter.h
@@ -0,0 +1,15 @@
+//
+//  DataCenter.h
+//  东北新闻网
+//
+//  Created by tenyea on 13-12-23.
+//  Copyright (c) 2013年 佐筱猪. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataCenter : NSObject
+
+
++ (DataCenter*)sharedCenter;
+@end
---
+++ PetInfo/PetInfo/DataCenter.m
@@ -0,0 +1,81 @@
+//
+//  DataCenter.m
+//  东北新闻网
+//
+//  Created by tenyea on 13-12-23.
+//  Copyright (c) 2013年 佐筱猪. All rights reserved.
+//
+
+#import "DataCenter.h"
+static DataCenter *sigleton = nil;
+@implementation DataCenter
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//实例化
++(DataCenter *)sharedCenter{
+    
+    if (sigleton == nil) {
+        @synchronized(self){
+            sigleton = [[DataCenter alloc] init];
+        }
+    }
+    return sigleton;
+
+}
+//限制当前对象创建多实例
+#pragma mark - sengleton setting
++ (id)allocWithZone:(NSZone *)zone {
+    @synchronized(self) {
+        if (sigleton == nil) {
+            sigleton = [super allocWithZone:zone];
+        }
+    }
+    return sigleton;
+}
+
++ (id)copyWithZone:(NSZone *)zone {
+    return self;
+}
+
+- (id)retain {
+    return self;
+}
+
+- (unsigned)retainCount {
+    return UINT_MAX;
+}
+
+- (oneway void)release {
+}
+
+- (id)autorelease {
+    return self;
+}
+
+@end
