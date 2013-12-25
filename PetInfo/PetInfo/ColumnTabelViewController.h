f30b77027e2886bfc9c14c148d446721dea344a3
zzw19880707 <tenyea@tenyeatekiMac-mini.local>
2013-12-25 17:54:25 +0800

    修改栏目编辑页面

---
+++ PetInfo/PetInfo/ColumnTabelViewController.h
@@ -0,0 +1,20 @@
+//
+//  ColumnTabelViewController.h
+//  东北新闻网
+//
+//  Created by tenyea on 13-12-24.
+//  Copyright (c) 2013年 佐筱猪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ColumnTabelViewController : UITableViewController
+{
+
+    //显示的栏目
+    NSMutableArray *_showNameArray;
+    //不显示的栏目
+    NSMutableArray *_addNameArray;
+
+}
+@end
