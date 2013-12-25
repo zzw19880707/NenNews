439d5f1849e7599eefe622c96363fdac66a18b1c
zzw19880707 <tenyea@tenyeatekiMac-mini.local>
2013-12-25 17:57:50 +0800

    增加数据中心类，增加判断缓存文件大小、清空缓存、删除指定目录文件、判断指定目录文件大小的方法

--- PetInfo/PetInfo/DataCenter.h
+++ PetInfo/PetInfo/DataCenter.h
@@ -12,4 +12,13 @@
 
 
 + (DataCenter*)sharedCenter;
+//获取cache大小
+- (NSUInteger)cacheSize;
+//- (void)cacheData;
+//清空缓存
+- (void)cleanCache;
+//按照目录删除文件
++(void) deleteAllFilesInDir:(NSString*)path;
+//获取路径下文件大小
++(float)fileSizeForDir:(NSString*)path;
 @end
--- PetInfo/PetInfo/DataCenter.m
+++ PetInfo/PetInfo/DataCenter.m
@@ -7,10 +7,77 @@
 //
 
 #import "DataCenter.h"
+#import "FileUrl.h"
 static DataCenter *sigleton = nil;
 @implementation DataCenter
 
+//获取cache大小
+- (NSUInteger)cacheSize{
+        float size = [DataCenter fileSizeForDir:[FileUrl getCacheFile]];
+    return size;
+}
 
+//获取路径下文件大小
++(float)fileSizeForDir:(NSString*)path//计算文件夹下文件的总大小
+{
+    NSFileManager *fileManager = [[NSFileManager alloc] init];
+    float size =0;
+    NSArray* array = [fileManager contentsOfDirectoryAtPath:path error:nil];
+    for(int i = 0; i<[array count]; i++)
+    {
+        NSString *fullPath = [path stringByAppendingPathComponent:[array objectAtIndex:i]];
+        
+        BOOL isDir;
+        //判断是否是文件夹
+        if ( !([fileManager fileExistsAtPath:fullPath isDirectory:&isDir] && isDir) )
+        {
+            NSDictionary *fileAttributeDic=[fileManager attributesOfItemAtPath:fullPath error:nil];
+            size+= fileAttributeDic.fileSize;
+        }
+        else
+        {
+            size+=[self fileSizeForDir:fullPath];
+        }
+    }
+    [fileManager release];
+    return size;
+}
+//- (void)cacheData;
+//清空缓存
+- (void)cleanCache{
+    NSString *cachesDir = [FileUrl getCacheFile];
+    [DataCenter deleteAllFilesInDir:cachesDir];
+}
+//按照目录删除文件
++(void) deleteAllFilesInDir:(NSString*)path {
+	NSError* err=nil;
+//	NSArray* files = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:path error:&err];
+//	if (err!=nil) {
+//		return;
+//	}
+//	for (NSString* file in files) {
+//		NSString* filepath = [NSString stringWithFormat:@"%@%@",path,file];
+//		[[NSFileManager defaultManager] removeItemAtPath:filepath error:&err];
+//	}
+    NSFileManager *fileManager = [[NSFileManager alloc] init];
+    NSArray* array = [fileManager contentsOfDirectoryAtPath:path error:nil];
+    for(int i = 0; i<[array count]; i++)
+    {
+        NSString *fullPath = [path stringByAppendingPathComponent:[array objectAtIndex:i]];
+        
+        BOOL isDir;
+        //判断是否是文件夹
+        if ( !([fileManager fileExistsAtPath:fullPath isDirectory:&isDir] && isDir) )
+        {
+            [fileManager removeItemAtPath:fullPath error:&err];
+        }
+        else
+        {
+            [self deleteAllFilesInDir:fullPath];
+        }
+    }
+    [fileManager release];
+}
 
 
 
