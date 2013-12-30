226204882f1265d42630088283977336d31cbf0b
zzw19880707 <tenyea@tenyeatekiMac-mini.local>
2013-12-30 21:31:04 +0800

    增加相关model

---
+++ PetInfo/NewsContentModel.h
@@ -0,0 +1,15 @@
+//
+//  NewsContentModel.h
+//  东北新闻网
+//
+//  Created by tenyea on 13-12-30.
+//  Copyright (c) 2013年 佐筱猪. All rights reserved.
+//
+
+#import "BaseModel.h"
+
+@interface NewsContentModel : BaseModel
+
+//@property  
+
+@end
