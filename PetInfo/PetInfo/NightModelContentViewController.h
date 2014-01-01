480388d9ff4cb2665bb898d1c3115cf1809b9e90
zzw19880707 <zzw414851474@qq.com>
2014-01-01 23:29:06 +0800

    增加内容页

---
+++ PetInfo/PetInfo/NightModelContentViewController.h
@@ -0,0 +1,14 @@
+//
+//  NightModelContentViewController.h
+//  东北新闻网
+//
+//  Created by 佐筱猪 on 14-1-1.
+//  Copyright (c) 2014年 佐筱猪. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface NightModelContentViewController : BaseViewController
+
+@property (nonatomic,assign) NSInteger titleID;
+@end
---
+++ PetInfo/PetInfo/NightModelContentViewController.m
@@ -0,0 +1,38 @@
+//
+//  NightModelContentViewController.m
+//  东北新闻网
+//
+//  Created by 佐筱猪 on 14-1-1.
+//  Copyright (c) 2014年 佐筱猪. All rights reserved.
+//
+
+#import "NightModelContentViewController.h"
+
+@interface NightModelContentViewController ()
+
+@end
+
+@implementation NightModelContentViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
