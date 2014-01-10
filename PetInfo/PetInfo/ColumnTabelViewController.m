658800851a34c5fe8c60c819381341d05b5caaea
zzw19880707 <zzw414851474@qq.com>
2014-01-10 10:56:55 +0800

    去除多余空格

--- PetInfo/PetInfo/ColumnTabelViewController.m
+++ PetInfo/PetInfo/ColumnTabelViewController.m
@@ -140,7 +140,6 @@
     return cell;
 }
 
-
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
