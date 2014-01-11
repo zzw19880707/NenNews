4293f64bf500ec3c60335e1fd85263ef6608fd64
zzw19880707 <zzw414851474@qq.com>
2014-01-11 21:22:48 +0800

    新增夜间模式textview类

---
+++ PetInfo/PetInfo/NightModelTextView.m
@@ -0,0 +1,31 @@
+//
+//  NightModelTextView.m
+//  东北新闻网
+//
+//  Created by tenyea on 14-1-11.
+//  Copyright (c) 2014年 佐筱猪. All rights reserved.
+//
+
+#import "NightModelTextView.h"
+
+@implementation NightModelTextView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
