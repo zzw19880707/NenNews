a03b9532aadc3d0b8b872979dec42a76215898fd
zzw19880707 <zzw414851474@qq.com>
2014-01-17 17:22:47 +0800

    增加时间判断 来自动刷新

--- PetInfo/PetInfo/RootViewController.m
+++ PetInfo/PetInfo/RootViewController.m
@@ -11,9 +11,10 @@
 #import "FileUrl.h"
 #import "WebViewController.h"
 #import "NightModelContentViewController.h"
-
-@interface RootViewController ()
-
+#import "ThemeManager.h"
+@interface RootViewController (){
+    BOOL _enable;
+}
 @end
 
 @implementation RootViewController
@@ -30,20 +31,20 @@
 -(NSArray *)_initButton {
     
     //读取全部显示菜单
-
+    
     NSString *pathName = [[FileUrl getDocumentsFile] stringByAppendingPathComponent:column_show_file_name];
     //栏目数组
     NSMutableArray *nameArrays = [[NSMutableArray alloc]initWithContentsOfFile:pathName];
-//    用于存放按钮
+    //    用于存放按钮
     NSMutableArray *buttonArrays = [[NSMutableArray alloc]init];
-
-//    用于存放tableview
+    
+    //    用于存放tableview
     NSMutableArray *tableArrays = [[NSMutableArray alloc]init];
     for (int i =0; i<nameArrays.count; i++) {
-        int columnId = [[nameArrays[i] objectForKey:@"cloumID"] intValue];
+        int columnId = [[nameArrays[i] objectForKey:@"columnId"] intValue];
         UIButton *button = [Uifactory createButton:[nameArrays[i] objectForKey:@"name"]];
         button.frame = CGRectMake(10 + 70*i, 0, 60, 30);
-
+        
         button.contentHorizontalAlignment = UIControlContentHorizontalAlignmentCenter;
         button.tag = 1000+ i;
         [buttonArrays addObject:button];
@@ -55,7 +56,7 @@
         newsTableView.changeDelegate = self;
         newsTableView.type = 0;
         NSMutableDictionary *d = [[NSMutableDictionary alloc]initWithContentsOfFile:[[FileUrl getDocumentsFile]stringByAppendingPathComponent:data_file_name]];
-
+        
         NSMutableDictionary *dic = [d objectForKey:[NSString stringWithFormat:@"%d",columnId]];
         if (dic.count>0) {
             NSMutableArray *data = [[NSMutableArray alloc]init];
@@ -98,8 +99,8 @@
     self.navigationItem.leftBarButtonItem= [leftitem autorelease];
     UIBarButtonItem *rightItem = [[UIBarButtonItem alloc]initWithCustomView:rightButton];
     self.navigationItem.rightBarButtonItem= [rightItem autorelease];
-
-
+    
+    
 }
 #pragma mark UIScrollViewEventDelegate
 -(void)addButtonAction{
@@ -110,13 +111,14 @@
 }
 -(void)showRightMenu{
     [self.appDelegate.menuCtrl showRightController:YES];
-
+    
 }
 -(void)showLeftMenu{
     [self.appDelegate.menuCtrl showLeftController:YES];
-
+    
 }
 -(void)setEnableGesture:(BOOL)b{
+    _enable = b;
     [self.appDelegate.menuCtrl setEnableGesture:b];
 }
 -(void)autoRefreshData:(NewsNightModelTableView *)tableView{
@@ -126,16 +128,21 @@
 #pragma mark UI
 - (void)viewDidLoad
 {
+    
     [super viewDidLoad];
     [self _initNavagationbarButton];
-
+    
     _sc = [[BaseScrollView alloc]initwithButtons:[self _initButton] WithFrame:CGRectMake(0, 0, 320, ScreenHeight)];
     _sc.eventDelegate = self;
     [self.view addSubview:_sc];
     
     
     NewsNightModelTableView *table  = (NewsNightModelTableView *) VIEWWITHTAG( VIEWWITHTAG(_sc, 10001), 1300);
-    [table autoRefreshData];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"yyMMddHHmm"];
+    _po([formatter stringFromDate:[NSDate date]]);
+    int data =[[formatter stringFromDate:[NSDate date]] intValue];
+    table.lastDate = data;
     [self getData:table];
 }
 -(void)viewWillAppear:(BOOL)animated{
@@ -143,7 +150,7 @@
     [super viewWillAppear:animated];
     
     //    开启左滑、右滑菜单
-    [self.appDelegate.menuCtrl setEnableGesture:YES];
+    [self.appDelegate.menuCtrl setEnableGesture:NO];
 }
 
 -(void)viewWillDisappear:(BOOL)animated{
@@ -163,13 +170,25 @@
         NightModelContentViewController *nightModel = [[NightModelContentViewController alloc]init];
         nightModel.newsId = [imageData[index] objectForKey:@"newsId"] ;
         nightModel.type = [[imageData[index] objectForKey:@"type"] intValue];
+        nightModel.newsAbstract = [imageData[index] objectForKey:@"newsAbstract"] ;
+        nightModel.ImageUrl =[imageData[index] objectForKey:@"img"];
         [self.navigationController pushViewController:nightModel animated:YES];
     }
 }
+
+-(ColumnModel *)addisselected :(ColumnModel *)model {
+    NSString *newsId = model.newsId;
+    FMResultSet *rs =[self.db executeQuery:@"select * from columnData where newsId = ?",newsId];
+    while (rs.next) {
+        model.isselected = YES;
+        return model;
+    }
+    return model;
+}
 #pragma mark UItableviewEventDelegate
 -(void)getData :(NewsNightModelTableView *)tableView{
-    if ([self getConnectionAlert]) {
-
+    [self getConnectionAlert];
+    
     //    参数
     NSMutableDictionary *params  = [[NSMutableDictionary alloc]init];
     int count = [[NSUserDefaults standardUserDefaults]integerForKey:kpageCount];
@@ -182,41 +201,37 @@
         NSString *sinceID = model.newsId;
         [params setValue:sinceID forKey:@"maxId"];
     }
-    
-    //    [params setValue:<#(id)#> forKey:@"maxId"];
-        [DataService requestWithURL:URL_getColumn_List andparams:params andhttpMethod:@"POST" completeBlock:^(id result) {
-            NSArray *array =  [result objectForKey:@"data"];
-            NSMutableArray *listData = [[NSMutableArray alloc]init];
-            
-            for (NSDictionary *dic  in array) {
-                ColumnModel * model = [[ColumnModel alloc]initWithDataDic:dic];
-                [listData addObject:model];
-            }
-            tableView.isMore = true;
-            
-            [listData addObjectsFromArray:tableView.data];
-            tableView.data =listData;
-            tableView.imageData = [result objectForKey:@"picture"];
-            [tableView reloadData];
+    [DataService requestWithURL:URL_getColumn_List andparams:params andhttpMethod:@"GET" completeBlock:^(id result) {
+        tableView.isMore = true;
+        NSArray *array =  [result objectForKey:@"data"];
+        if (array.count ==0) {
             [tableView doneLoadingTableViewData];
-            
-        } andErrorBlock:^(NSError *error) {
-            [tableView doneLoadingTableViewData];
-        }];
-    }else{
+            return ;
+        }
+        NSMutableArray *listData = [[NSMutableArray alloc]init];
+        
+        for (NSDictionary *dic  in array) {
+            ColumnModel * model = [[ColumnModel alloc]initWithDataDic:dic];
+            model = [self addisselected:model];
+            [listData addObject:model];
+        }
+        
+        [listData addObjectsFromArray:tableView.data];
+        tableView.data =listData;
+        tableView.imageData = [result objectForKey:@"picture"];
+        [tableView reloadData];
         [tableView doneLoadingTableViewData];
-
-    }
+        
+    } andErrorBlock:^(NSError *error) {
+        [tableView doneLoadingTableViewData];
+    }];
     
-
 }
+
 //上拉刷新
 -(void)pullDown:(NewsNightModelTableView *)tableView{
-    if ([self getConnectionAlert]) {
-        [self getData:tableView];
-    }else{
-        [tableView doneLoadingTableViewData];
-    }
+    [self getData:tableView];
+    
 }
 //下拉加载
 -(void)pullUp:(NewsNightModelTableView *)tableView{
@@ -233,35 +248,35 @@
         NSString *sinceID = model.newsId;
         [params setValue:sinceID forKey:@"sinceId"];
     }
-    if ([self getConnectionAlert]) {
-        [DataService requestWithURL:URL_getColumn_List andparams:params andhttpMethod:@"POST" completeBlock:^(id result) {
-            NSArray *array =  [result objectForKey:@"data"];
-            NSArray *imageArray = [result objectForKey:@"picture"];
-            NSMutableArray *listData = [[NSMutableArray alloc]init];
-            
-            for (NSDictionary *dic  in array) {
-                ColumnModel * model = [[ColumnModel alloc]initWithDataDic:dic];
-                [listData addObject:model];
-            }
-            
-            [tableView doneLoadingTableViewData];
-            NSMutableArray *arr = [NSMutableArray arrayWithArray:tableView.data];
-            [arr addObjectsFromArray:listData];
-            tableView.data  = arr;
-            tableView.imageData = imageArray;
-            if (listData.count < count*10) {
-                tableView.isMore = false;
-            }
-            [tableView reloadData];
-            
-        } andErrorBlock:^(NSError *error) {
-            [tableView doneLoadingTableViewData];
-            
-        }];
-    }else{
+    [self getConnectionAlert];
+    [DataService requestWithURL:URL_getColumn_List andparams:params andhttpMethod:@"GET" completeBlock:^(id result) {
+        NSArray *array =  [result objectForKey:@"data"];
+        NSArray *imageArray = [result objectForKey:@"picture"];
+        NSMutableArray *listData = [[NSMutableArray alloc]init];
+        
+        for (NSDictionary *dic  in array) {
+            ColumnModel * model = [[ColumnModel alloc]initWithDataDic:dic];
+            model = [self addisselected:model];
+            [listData addObject:model];
+        }
+        
         [tableView doneLoadingTableViewData];
-
-    }
+        NSMutableArray *arr = [NSMutableArray arrayWithArray:tableView.data];
+        [arr addObjectsFromArray:listData];
+        tableView.data  = arr;
+        if (imageArray.count !=0) {
+            tableView.imageData = imageArray;
+        }
+        if (listData.count < count*10) {
+            tableView.isMore = false;
+        }
+        [tableView reloadData];
+        
+    } andErrorBlock:^(NSError *error) {
+        [tableView doneLoadingTableViewData];
+        
+    }];
+    
     
     
 }
@@ -269,17 +284,12 @@
     if (tableView.imageData.count >0&&indexPath.section==0) {
         
     }else{
-        NightModelContentViewController *nightModel = [[NightModelContentViewController alloc]init];
         ColumnModel *model =tableView.data[indexPath.row];
-        nightModel.type = [model.type intValue];
-        nightModel.newsId = [NSString stringWithFormat:@"%@",model.newsId];
-        [self.navigationController pushViewController:nightModel animated:YES];
+        [self pushNewswithColumn:model];
     }
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-
+    
 }
-
-
 
 #pragma mark 按钮事件
 - (void)leftAction {
@@ -287,13 +297,20 @@
 }
 - (void)rightAction {
     [self.appDelegate.menuCtrl showRightController:YES];
-
+    
 }
 
 
-#pragma mark columnchangeDelegate 
+#pragma mark columnchangeDelegate
 -(void)columnChanged:(NSArray *)array{
     _sc.buttonsNameArray = [self _initButton];
+    NewsNightModelTableView *table  = (NewsNightModelTableView *) VIEWWITHTAG( VIEWWITHTAG(_sc, 10001), 1300);
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"yyMMddHHmm"];
+    _po([formatter stringFromDate:[NSDate date]]);
+    int data =[[formatter stringFromDate:[NSDate date]] intValue];
+    table.lastDate = data;
+    [self getData:table];
 }
 #pragma mark 内存管理
 - (void)didReceiveMemoryWarning
