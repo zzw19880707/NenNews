2d9602925b719eab9b66624a5c8c215a6160e6dd
zzw19880707 <zzw414851474@qq.com>
2014-01-26 16:32:44 +0800

    修改数据模型

--- PetInfo/ColumnModel.h
+++ PetInfo/ColumnModel.h
@@ -12,8 +12,14 @@
 
 @property (nonatomic,copy) NSString *newsId;
 @property (nonatomic,copy) NSNumber *type;
+@property (nonatomic,copy) NSNumber *typeUp;//右上标(0:无 1：独家 2：原创 3：推荐)
+
 @property (nonatomic,copy) NSString *newsAbstract;
 @property (nonatomic,copy) NSString *title;
 @property (nonatomic,copy) NSString *img;
+@property (nonatomic,copy) NSString *img1;//图片新闻
+@property (nonatomic,copy) NSString *img2;//图片新闻
+@property (nonatomic,copy) NSString *img3;//图片新闻
+
 @property (nonatomic,assign) BOOL isselected;
 @end
