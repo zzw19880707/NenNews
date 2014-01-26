01318d3d1b4d9eb84d8f90e207fb27b25e4a0f8e
zzw19880707 <zzw414851474@qq.com>
2014-01-26 16:26:02 +0800

    将cell 的夜间模式抽离成基类

---
+++ PetInfo/PetInfo/BaseNightModelCell.h
@@ -0,0 +1,13 @@
+//
+//  BaseNightModelCell.h
+//  东北新闻网
+//
+//  Created by tenyea on 14-1-26.
+//  Copyright (c) 2014年 佐筱猪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseNightModelCell : UITableViewCell
+-(NSString *) getConnectionAvailable;
+@end
