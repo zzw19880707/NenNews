b85af1f71943d68ccaf4c90d23a95736b8d47aec
zzw19880707 <zzw414851474@qq.com>
2014-02-21 10:23:21 +0800

    修改相关分享key

--- PetInfo1.0.2/PetInfo/AppDelegate.m
+++ PetInfo1.0.2/PetInfo/AppDelegate.m
@@ -32,17 +32,17 @@
      连接新浪微博开放平台应用以使用相关功能，此应用需要引用SinaWeiboConnection.framework
      http://open.weibo.com上注册新浪微博开放平台应用，并将相关信息填写到以下字段
      **/
-    [ShareSDK connectSinaWeiboWithAppKey:@"246405418"
-                               appSecret:@"3dac8f882665f6270ecffa5a5c90eed5"
-                             redirectUri:@"http://www.daoyoudao.com" weiboSDKCls:[WeiboSDK class]] ;
+    [ShareSDK connectSinaWeiboWithAppKey:@"1013662903"
+                               appSecret:@"4279d2d491e22911018abd8dff9e7d57"
+                             redirectUri:@"http://www.sharesdk.cn" weiboSDKCls:[WeiboSDK class]] ;
     /**
      连接腾讯微博开放平台应用以使用相关功能，此应用需要引用TencentWeiboConnection.framework
      http://dev.t.qq.com上注册腾讯微博开放平台应用，并将相关信息填写到以下字段
      
      如果需要实现SSO，需要导入libWeiboSDK.a，并引入WBApi.h，将WBApi类型传入接口
      **/
-    [ShareSDK connectTencentWeiboWithAppKey:@"801307650"
-                                  appSecret:@"ae36f4ee3946e1cbb98d6965b0b2ff5c"
+    [ShareSDK connectTencentWeiboWithAppKey:@"801480021"
+                                  appSecret:@"1d7424cfb76f8c2b5719ad4aaf88dd4b"
                                 redirectUri:@"http://www.sharesdk.cn"
                                    wbApiCls:[WeiboApi class]];
     
@@ -55,8 +55,8 @@
      
      如果需要实现SSO，需要导入TencentOpenAPI.framework,并引入QQApiInterface.h和TencentOAuth.h，将QQApiInterface和TencentOAuth的类型传入接口
      **/
-    [ShareSDK connectQZoneWithAppKey:@"101007496"
-                           appSecret:@"fa44d81770c1444d42a171f547fcdfc1"
+    [ShareSDK connectQZoneWithAppKey:@"101019244"
+                           appSecret:@"284d2fcd638a76bfb7802eedb73fe852"
                    qqApiInterfaceCls:[QQApiInterface class]
                      tencentOAuthCls:[TencentOAuth class]];
     
@@ -66,26 +66,25 @@
      **/
     [ShareSDK connectWeChatWithAppId:@"wx6b665e9bc0f77e41" wechatCls:[WXApi class]];
     
-    //    /**
-    //     连接QQ应用以使用相关功能，此应用需要引用QQConnection.framework和QQApi.framework库
-    //     http://mobile.qq.com/api/上注册应用，并将相关信息填写到以下字段
-    //     **/
-    //    //旧版中申请的AppId（如：QQxxxxxx类型），可以通过下面方法进行初始化
-    //    //    [ShareSDK connectQQWithAppId:@"QQ075BCD15" qqApiCls:[QQApi class]];
-    //
-    //    [ShareSDK connectQQWithQZoneAppKey:@"100371282"
-    //                     qqApiInterfaceCls:[QQApiInterface class]
-    //                       tencentOAuthCls:[TencentOAuth class]];
-    
-    
+    /**
+     连接QQ应用以使用相关功能，此应用需要引用QQConnection.framework和QQApi.framework库
+     http://mobile.qq.com/api/上注册应用，并将相关信息填写到以下字段
+     **/
+    //旧版中申请的AppId（如：QQxxxxxx类型），可以通过下面方法进行初始化
+    //    [ShareSDK connectQQWithAppId:@"QQ075BCD15" qqApiCls:[QQApi class]];
+
+    [ShareSDK connectQQWithQZoneAppKey:@"101019244"
+                     qqApiInterfaceCls:[QQApiInterface class]
+                       tencentOAuthCls:[TencentOAuth class]];
+
     
     /**
      连接人人网应用以使用相关功能，此应用需要引用RenRenConnection.framework
      http://dev.renren.com上注册人人网开放平台应用，并将相关信息填写到以下字段
      **/
-    [ShareSDK connectRenRenWithAppId:@"246729"
-                              appKey:@"ab880b1815274cc8ba554bf1398f67f3"
-                           appSecret:@"6e7382e8f6034b158d14f0c4a165ea99"
+    [ShareSDK connectRenRenWithAppId:@"246289"
+                              appKey:@"ec7163b7094d404eb4bddfa1803681fe"
+                           appSecret:@"655df8b512f94a6f961b448da5ce119d"
                    renrenClientClass:[RennClient class]];
 }
 #pragma mark 入口
