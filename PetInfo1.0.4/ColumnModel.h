8be13b1122e278dce8c04147a00e947c79905687
zzw19880707 <zzw414851474@qq.com>
2014-04-26 09:18:26 +0800

    增加相关注释和栏目类型

--- PetInfo1.0.4/ColumnModel.h
+++ PetInfo1.0.4/ColumnModel.h
@@ -11,7 +11,8 @@
 @interface ColumnModel : BaseModel
 
 @property (nonatomic,copy) NSString *newsId;
-@property (nonatomic,copy) NSNumber *type;
+@property (nonatomic,copy) NSNumber *columnType;//(0:综合新闻 1：专题 2：图集 3：视频 )
+@property (nonatomic,copy) NSNumber *type;//右下标(0:普通新闻 1：专题 2：图集 3：视频 4：推广)
 @property (nonatomic,copy) NSNumber *typeUp;//右上标(0:无 1：独家 2：原创 3：推荐)
 
 @property (nonatomic,copy) NSString *newsAbstract;
@@ -21,6 +22,7 @@
 @property (nonatomic,copy) NSString *img2;//图片新闻
 @property (nonatomic,copy) NSString *img3;//图片新闻
 
-@property (nonatomic,copy) NSString *ADURL;//广告地址
+@property (nonatomic,copy) NSString *AdUrl;//广告地址
 @property (nonatomic,assign) BOOL isselected;
+
 @end
